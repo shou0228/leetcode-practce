e31b6fca0939da82b8de1c1ccd4ff70756df4a6c
shouchen <68886395+shou0228@users.noreply.github.com>
2022-04-30 00:55:33 +0800

    Update and rename 217-1.java to leetcode_217.java

---
+++ leetcode_217.java
@@ -0,0 +1,53 @@
+#Contains Duplicate
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+#暴力解法
+public boolean containsDuplicate_bruteforce(int[] nums) {
+   if (nums == null || nums.length == 0) return false;
+   
+   int l = nums.length;
+   for (int i=0; i<l; i++) {
+      for (int j=i+1; j<l; j++) {
+         if (nums[i] == nums[j]) {
+            return true;
+         }
+      }
+   }
+   return false;
+}
+#O(n^2)
+------------------------------------------------------------------------------------------------------
+####################
+#排序解法
+public boolean containsDuplicate(int[] nums) {
+   if (nums == null || nums.length == 0) return false;
+   
+   Arrays.sort(nums);
+   int l = nums.length;
+   for (int i=1; i<l; i++) {
+      if (nums[i-1] == nums[i]) {
+         return true;
+      }
+   }
+   return false;
+#O(nlogn)O(nlogn)
+ ------------------------------------------------------------------------------------------------------
+ public boolean containsDuplicate_extraMemory(int[] nums) {
+   if (nums == null || nums.length == 0) return false;
+   
+   Set<Integer> set = new HashSet<>();
+   int l = nums.length;
+   for (int i=0; i<l; i++) {
+      if (set.contains(nums[i])) {
+         return true;
+      }
+      set.add(nums[i]);
+   }
+   return false;
+}
+#O(1)
+
+
+
+ 
+
+   
