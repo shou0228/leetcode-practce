28452b1d105b2a341ccaa6c084cb009b7c0286ee
shouchen <68886395+shou0228@users.noreply.github.com>
2022-06-02 23:15:57 +0800

    Update leetcode_1_Two sum.py

--- python/leetcode_1_Two sum.py
+++ python/leetcode_1_Two sum.py
@@ -2,7 +2,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        # 因為要返回的是索引，所以應該使用陣列的索引進行迴圈 
+        # 因為要返回的是索引，所以應該使用陣列的索引進行兩次for迴圈 跑兩次分別找出兩個相加為9的數字
         for i in range(n):
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
